50ea7bab9e24b195294f74ad6d82c3d98228f6c7
Jeanette93Cph <jeanette.93@live.dk>
2022-10-13 13:26:36 +0200

    Task 1 og 2
    uge 41

---
+++ src/ChanceCard.java
@@ -0,0 +1,34 @@
+public class ChanceCard
+{
+    String name;
+    int cost;
+    int payout;
+
+
+    public ChanceCard(String name, int cost, int payout)
+    {
+        this.name = name;
+        this.cost = cost;
+        this.payout = payout;
+    }
+
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getCost()
+    {
+        return cost;
+    }
+
+    public int getPayout()
+    {
+        return payout;
+    }
+
+
+
+}
